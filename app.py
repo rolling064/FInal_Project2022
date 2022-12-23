5d185b92dd5b8ccf73df0d4ca8a9b0997b970728
rolling064 <121214893+rolling064@users.noreply.github.com>
2022-12-23 22:51:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,115 @@
+import os
+import sys
+
+from flask import Flask, request, abort, send_file
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,MessageTemplateAction
+
+from utils import send_text_message,send_button_message,send_image_message
+from fsm import TocMachine
+
+load_dotenv()
+
+machine = TocMachine(
+    states=['user','1st_level_folder','password','secret','ncku_folder','math','csie','nckuedu','entertainment_folder','facebook','youtube','twich','google_folder','translation','drive','gmail','end'],
+    transitions=[{'trigger': 'advance', 'source': 'user', 'dest':'1st_level_folder', 'conditions':'is_going_to_1st_level_folder'},
+                 {'trigger': 'advance', 'source': 'user', 'dest':'password', 'conditions':'is_going_to_password'},
+                 {'trigger': 'advance', 'source': 'password', 'dest':'secret', 'conditions':'is_going_to_secret'},
+
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'ncku_folder', 'conditions':'is_going_to_ncku_folder'},
+                 
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'ncku_folder', 'conditions':'is_going_to_ncku_folder'},
+                 {'trigger': 'advance', 'source': 'ncku_folder', 'dest':'1st_level_folder', 'conditions':'back_to_1st_level_folder'},
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'entertainment_folder', 'conditions':'is_going_to_entertainment_folder'},
+                 {'trigger': 'advance', 'source': 'entertainment_folder', 'dest':'1st_level_folder', 'conditions':'back_to_1st_level_folder'},
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'google_folder', 'conditions':'is_going_to_google_folder'},
+                 {'trigger': 'advance', 'source': 'google_folder', 'dest':'1st_level_folder', 'conditions':'back_to_1st_level_folder'},
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'end', 'conditions':'is_going_to_end'},
+
+                 {'trigger': 'advance', 'source': 'ncku_folder', 'dest':'math', 'conditions':'is_going_to_math'},
+                 {'trigger': 'advance', 'source': 'math', 'dest':'ncku_folder', 'conditions':'back_to_ncku_folder'},
+                 {'trigger': 'advance', 'source': 'ncku_folder', 'dest':'csie', 'conditions':'is_going_to_csie'},
+                 {'trigger': 'advance', 'source': 'csie', 'dest':'ncku_folder', 'conditions':'back_to_ncku_folder'},
+                 {'trigger': 'advance', 'source': 'ncku_folder', 'dest':'nckuedu', 'conditions':'is_going_to_nckuedu'},
+                 {'trigger': 'advance', 'source': 'nckuedu', 'dest':'ncku_folder', 'conditions':'back_to_ncku_folder'},
+
+                 {'trigger': 'advance', 'source': 'entertainment_folder', 'dest':'facebook', 'conditions':'is_going_to_facebook'},
+                 {'trigger': 'advance', 'source': 'facebook', 'dest':'entertainment_folder', 'conditions':'back_to_entertainment_folder'},
+                 {'trigger': 'advance', 'source': 'entertainment_folder', 'dest':'youtube', 'conditions':'is_going_to_youtube'},
+                 {'trigger': 'advance', 'source': 'youtube', 'dest':'entertainment_folder', 'conditions':'back_to_entertainment_folder'},
+                 {'trigger': 'advance', 'source': 'entertainment_folder', 'dest':'twich', 'conditions':'is_going_to_twich'},
+                 {'trigger': 'advance', 'source': 'twich', 'dest':'entertainment_folder', 'conditions':'back_to_entertainment_folder'},
+
+                 {'trigger': 'advance', 'source': 'google_folder', 'dest':'translation', 'conditions':'is_going_to_translation'},
+                 {'trigger': 'advance', 'source': 'translation', 'dest':'google_folder', 'conditions':'back_to_google_folder'},
+                 {'trigger': 'advance', 'source': 'google_folder', 'dest':'drive', 'conditions':'is_going_to_drive'},
+                 {'trigger': 'advance', 'source': 'drive', 'dest':'google_folder', 'conditions':'back_to_google_folder'},
+                 {'trigger': 'advance', 'source': 'google_folder', 'dest':'gmail', 'conditions':'is_going_to_gmail'},
+                 {'trigger': 'advance', 'source': 'gmail', 'dest':'google_folder', 'conditions':'back_to_google_folder'},
+
+                 {'trigger': 'go_back', 'source': ['math','csie','nckuedu','facebook','youtube','twich','translation','drive','gmail','end','secret',], 'dest':'user',},
+                
+                 {'trigger': 'advance', 'source': '1st_level_folder', 'dest':'1st_level_folder'},
+                 {'trigger': 'advance', 'source': 'ncku_folder', 'dest':'ncku_folder'},
+                 {'trigger': 'advance', 'source': 'entertainment_folder', 'dest':'entertainment_folder'},
+                 {'trigger': 'advance', 'source': 'google_folder', 'dest':'google_folder'},
+                ],
+    initial="user",
+    auto_transitions=False,
+    show_conditions=True
+)
+
+app = Flask(__name__, static_url_path='')
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token =os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+@app.route("/callback", methods=["POST"])
+def webhook_handler():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+ 
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+ 
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if not isinstance(event.message.text, str):
+            continue
+        print(f"\nFSM STATE: {machine.state}")
+        print(f"REQUEST BODY: \n{body}")
+        response = machine.advance(event)
+        if response == False:
+            if machine.state == 'password':
+                send_text_message(event.reply_token, '密碼錯誤，請重新嘗試')
+            else:
+                send_text_message(event.reply_token, '目前只提供資料夾的功能\n輸入"start"便可開啟資料夾')
+
+    return "OK"
+
+@app.route("/show-fsm", methods=["GET"])
+def show_fsm():
+    machine.get_graph().draw("fsm.png", prog="dot", format="png")
+    return send_file("fsm.png", mimetype="image/png")
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port, debug=True)
---
+++ fsm.py
@@ -0,0 +1,230 @@
+from transitions.extensions import GraphMachine
+from linebot.models import MessageTemplateAction
+import requests
+import pandas as pd
+
+from utils import send_text_message,send_button_message,send_image_message
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(model=self, **machine_configs)
+
+    def is_going_to_password(self, event):
+        text = event.message.text
+        return text.lower() == '開啟資料夾'
+    def on_enter_password(self, event):
+        send_text_message(event.reply_token, "請輸入密碼")
+
+    def is_going_to_secret(self, event):
+        text = event.message.text
+        return text.lower() == 'rolling'
+    def on_enter_secret(self, event):
+        send_text_message(event.reply_token, "https://imgur.com/a/cTVg634")
+        self.go_back()
+
+    #第一層資料夾
+    def is_going_to_1st_level_folder(self, event):
+        text = event.message.text
+        return text.lower() == 'start' or text.lower() == '返回第一層資料夾'
+    def on_enter_1st_level_folder(self, event):
+        title = '第一層資料夾'
+        text = '請選擇想讀取的資料夾'
+        btn = [
+            MessageTemplateAction(
+                label = 'ncku資料夾',
+                text ='ncku'
+            ),
+            MessageTemplateAction(
+                label = '娛樂資料夾',
+                text = '娛樂'
+            ),
+            MessageTemplateAction(
+                label = 'google資料夾',
+                text = 'google'
+            ),
+            MessageTemplateAction(
+                label = '結束',
+                text = '結束'
+            ),
+        ]
+        send_button_message(event.reply_token, title, text, btn)
+
+
+    #結束
+    def is_going_to_end(self, event):
+        text = event.message.text
+        return text.lower() == '結束'
+    def on_enter_end(self, event):
+        send_text_message(event.reply_token, "謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+
+    #ncku資料夾
+    def is_going_to_ncku_folder(self, event):
+        text = event.message.text
+        return text.lower() == 'ncku' or text.lower() == '返回ncku資料夾'
+    def on_enter_ncku_folder(self, event):
+        title = 'ncku資料夾'
+        text = '請選擇網站'
+        btn = [
+            MessageTemplateAction(
+                label = '數學系',
+                text ='數學系'
+            ),
+            MessageTemplateAction(
+                label = '資訊系',
+                text = '資訊系'
+            ),
+            MessageTemplateAction(
+                label = '成功入口',
+                text = '成功入口'
+            ),
+            MessageTemplateAction(
+                label = '返回第一層資料夾',
+                text = '返回第一層資料夾'
+            ),
+        ]
+        send_button_message(event.reply_token, title, text, btn)
+
+
+    def is_going_to_math(self, event):
+        text = event.message.text
+        return text.lower() == '數學系'
+    def on_enter_math(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://www.math.ncku.edu.tw/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    def is_going_to_csie(self, event):
+        text = event.message.text
+        return text.lower() == '資訊系'
+    def on_enter_csie(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://www.csie.ncku.edu.tw/zh-hant/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()        
+
+    def is_going_to_nckuedu(self, event):
+        text = event.message.text
+        return text.lower() == '成功入口'
+    def on_enter_nckuedu(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://i.ncku.edu.tw/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+
+    #返回ncku資料夾
+    def back_to_ncku_folder(self,event):
+        text = event.message.text
+        return text.lower() == '返回ncku資料夾'
+
+    #返回第一層
+    def back_to_1st_level_folder(self,event):
+        text = event.message.text
+        return text.lower() == '返回第一層資料夾'
+
+
+    #google資料夾
+    def is_going_to_google_folder(self, event):
+        text = event.message.text
+        return text.lower() == 'google' or text.lower() == '返回google資料夾'
+    def on_enter_google_folder(self, event):
+        title = 'google資料夾'
+        text = '請選擇網站'
+        btn = [
+            MessageTemplateAction(
+                label = 'google翻譯',
+                text ='google翻譯'
+            ),
+            MessageTemplateAction(
+                label = 'google雲端',
+                text = 'google雲端'
+            ),
+            MessageTemplateAction(
+                label = 'gmail',
+                text = 'gmail'
+            ),
+            MessageTemplateAction(
+                label = '返回第一層資料夾',
+                text = '返回第一層資料夾'
+            ),
+        ]
+        send_button_message(event.reply_token, title, text, btn)
+
+
+    def is_going_to_translation(self, event):
+        text = event.message.text
+        return text.lower() == 'google翻譯'
+    def on_enter_translation(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://translate.google.com.tw/?hl=zh-TW\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    def is_going_to_drive(self, event):
+        text = event.message.text
+        return text.lower() == 'google雲端'
+    def on_enter_drive(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://drive.google.com/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    def is_going_to_gmail(self, event):
+        text = event.message.text
+        return text.lower() == 'gmail'
+    def on_enter_gmail(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://mail.google.com/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    #返回google資料夾
+    def back_to_google_folder(self,event):
+        text = event.message.text
+        return text.lower() == '返回google資料夾'
+
+
+    #娛樂資料夾
+    def is_going_to_entertainment_folder(self, event):
+        text = event.message.text
+        return text.lower() == '娛樂' or text.lower() == '返回娛樂資料夾'
+    def on_enter_entertainment_folder(self, event):
+        title = '娛樂資料夾'
+        text = '請選擇網站'
+        btn = [
+            MessageTemplateAction(
+                label = 'facebook',
+                text ='facebook'
+            ),
+            MessageTemplateAction(
+                label = 'youtube',
+                text = 'youtube'
+            ),
+            MessageTemplateAction(
+                label = 'twitch',
+                text = 'twitch'
+            ),
+            MessageTemplateAction(
+                label = '返回第一層資料夾',
+                text = '返回第一層資料夾'
+            ),
+        ]
+        send_button_message(event.reply_token, title, text, btn)
+
+
+    def is_going_to_facebook(self, event):
+        text = event.message.text
+        return text.lower() == 'facebook'
+    def on_enter_facebook(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://www.facebook.com/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    def is_going_to_youtube(self, event):
+        text = event.message.text
+        return text.lower() == 'youtube'
+    def on_enter_youtube(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://www.youtube.com/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    def is_going_to_twich(self, event):
+        text = event.message.text
+        return text.lower() == 'twitch'
+    def on_enter_twich(self, event):
+        send_text_message(event.reply_token, "以下是您要的網站\nhttps://www.twitch.tv/\n謝謝您的使用，歡迎你下次的使用")
+        self.go_back()
+
+    #返回娛樂資料夾
+    def back_to_entertainment_folder(self,event):
+        text = event.message.text
+        return text.lower() == '返回娛樂資料夾'
